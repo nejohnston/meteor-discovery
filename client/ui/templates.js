d47c49c45c045508716d30788a9d65e9fc12c1f2
njohnstud <nicholas.johnston94@gmail.com>
2018-02-05 11:32:40 -0800

    meteor npm install --save react react-dom

--- client/ui/templates.js
+++ client/ui/templates.js
@@ -1,15 +1,10 @@
 import { Template } from "meteor/templating";
 import { ReactiveVar } from "meteor/reactive-var";
 
-const messages = new ReactiveVar([
-  { author: "me", content: "aaaaaaa" },
-  { author: "me", content: "aaaaaaa" }
-]);
-
-// this gets all the messages
+// this queries the database for all Messages
 Template.chatwindow.helpers({
   messages() {
-    return messages.get();
+    return Messages.find();
   }
 });
 
@@ -18,11 +13,7 @@ Template.chatinput.events({
   "submit #chat-input form"(e) {
     e.preventDefault();
     const content = e.target[0].value;
-    content &&
-      messages.set([
-        ...messages.get(),
-        { author: "me", content }
-      ]);
+    content && Messages.insert({ author: "me", content });
     e.target[0].value = "";
   }
 });
--- collections.js
+++ collections.js
@@ -3,4 +3,5 @@ import { Mongo } from "meteor/mongo";
 // without importing messages, goes and finds them and then compiles them
 // isomorphic environment, clones code between the client and the server
 // meteor syncs the environment of code between client and server
-const Messages = new Mongo.Collection("messages");
+// accessible everywhere in app
+Messages = new Mongo.Collection("messages");
