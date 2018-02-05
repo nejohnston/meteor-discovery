0e20c353b64dc0e04bcbf6a32a32b9250ccbc744
njohnstud <nicholas.johnston94@gmail.com>
2018-02-05 10:37:27 -0800

    added .events to templates.js, on submit in chat-input form console.logs messages and pushes all new messages to that array

--- client/ui/templates.js
+++ client/ui/templates.js
@@ -1,13 +1,24 @@
 import { Template } from "meteor/templating";
 import { ReactiveVar } from "meteor/reactive-var";
 
+const messages = [
+  { author: "me", content: "aaaaaaa" },
+  { author: "me", content: "aaaaaaa" }
+];
+
 Template.chatwindow.helpers({
   messages() {
-    return [
-      { author: "me", content: "aaaaaaa" },
-      { author: "me", content: "aaaaaaa" }
-    ];
+    return messages;
   }
 });
 
-Template.chatinput.helpers();
+Template.chatinput.events({
+  "submit #chat-input form"(e) {
+    e.preventDefault();
+    const content = e.target[0].value;
+    messages.push({ author: "me", content });
+    console.log(messages);
+  }
+});
+
+Template.chatinput.helpers({});
