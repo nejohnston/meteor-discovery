06b8804c8801f7226fd042bb661ed0c57fce3d26
njohnstud <nicholas.johnston94@gmail.com>
2018-02-05 11:48:06 -0800

    added props to return in HelloWorld component

--- client/ui/components/HelloWorld.js
+++ client/ui/components/HelloWorld.js
@@ -2,10 +2,18 @@ import React, { Component } from "react";
 import { withTracker} from 'meteor/react-meteor-data'
 export default class HelloWorld extends Component {
   render() {
-    return <h1>Hello World</h1>;
+    return (<div>
+        <ul>
+        {this.props.messages.map(messages => {
+            <li key={messages._id}>
+            {message.author}: {message.content}</li>
+        })}
+        </ul>
+    </div>)
   }
 }
 
+//keeps track of server changes behind the scenes
 export default withTracker(() => {
     return {
         messages: Messages.find().fetch()
