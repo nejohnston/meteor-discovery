75bcee58dd32471c1126c13223390acd94d8ca0f
njohnstud <nicholas.johnston94@gmail.com>
2018-02-05 11:45:29 -0800

    added withTracker to HelloWorld.js to watch updates to server behind the scenes

--- client/ui/components/HelloWorld.js
+++ client/ui/components/HelloWorld.js
@@ -1,6 +1,13 @@
 import React, { Component } from "react";
+import { withTracker} from 'meteor/react-meteor-data'
 export default class HelloWorld extends Component {
   render() {
     return <h1>Hello World</h1>;
   }
 }
+
+export default withTracker(() => {
+    return {
+        messages: Messages.find().fetch()
+    }
+})(HelloWorld)
