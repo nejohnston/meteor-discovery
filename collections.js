755099a26f624477a4054505f4fcff60d52a2f86
njohnstud <nicholas.johnston94@gmail.com>
2018-02-05 10:54:59 -0800

    added notes to collections.js

--- collections.js
+++ collections.js
@@ -1,4 +1,6 @@
 import { Mongo } from "meteor/mongo";
 
 // without importing messages, goes and finds them and then compiles them
+// isomorphic environment, clones code between the client and the server
+// meteor syncs the environment of code between client and server
 const Messages = new Mongo.Collection("messages");
