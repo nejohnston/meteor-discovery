046fdece86e3e4ae04e625d56db71c1850d3efbb
njohnstud <nicholas.johnston94@gmail.com>
2018-02-05 10:25:11 -0800

    added helpers to templates.js so to enable chat-window to receive messages

--- client/ui/templates.js
+++ client/ui/templates.js
@@ -1,5 +1,12 @@
 import { Template } from "meteor/templating";
 
-Template.chatwindow;
+Template.chatwindow.helpers({
+  messages() {
+    return [
+      { author: "me", content: "aaaaaaa" },
+      { author: "me", content: "aaaaaaa" }
+    ];
+  }
+});
 
 Template.chatinput;
